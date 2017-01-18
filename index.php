38880bee79f4d4cb7d2230bd490cb9b1244fde6f
ChihWu <chihwu@bu.edu>
2017-01-17 22:48:08 -0500

    commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+	  <title>Raymond(ChihYung) Wu</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <link rel="stylesheet" href="jquery.circular-carousel.css" />
+	  <link rel="stylesheet" href="style.css">
+	  <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Roboto+Slab" rel="stylesheet">
+	  
+	</head>
+	<body>
+		<div class="header-deco-bar">
+		</div>
+
+		<div class="container">
+			<div class="nav">
+				Home
+			</div>
+
+			<div class="profile-img-div">
+				<div class="profile-img-decorator">
+					<img src="images/Ray_logo.png" />
+				</div>
+				<div class="profile-img-inner-div">
+					<img src="images/Raymond_black_white.png" />
+				</div>
+				<div class="profile-caption">
+					<p>HELLO, I'M RAYMOND</p>
+					<p>I LOVE CREATING WEBSITES</p>
+				</div>
+			</div>
+
+			<div class="section-div">
+				<div class="section-div-title-decorator">
+					<div class="section-div-title-decorator-bar"></div>
+					<div class="section-div-title">
+						<p>ABOUT</p>
+					</div>
+
+					<div>
+					</div>
+
+					<div class="section-div-content">
+						<div class="section-div-content-subdiv">
+							<div class="section-div-content-subdiv-title">
+								<p>ABOUT ME
+								</p>
+							</div>
+
+							<div class="section-div-content-subdiv-bar-decorator"></div>
+
+							<div class="section-div-content-subdiv-content">							
+								I have more than 4 years of experience of working a web developer and application developer. In addition, I also have abundant experience of working
+								with various graphic design tools such as Adobe Photoshop, Adobe Illustrator, and Adobe InDesign, to create 
+								many material for both web development and advertising for businesses.
+							</div>
+
+							<div class="section-div-content-subdiv-bar-decorator">
+							</div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="section-div-content-subdiv-title">
+								<p>WHAT I'VE DONE
+								</p>
+							</div>
+
+							<div class="section-div-content-subdiv-bar-decorator"></div>
+
+							<div class="section-div-content-subdiv-content">
+								My expertise is in both Computer Science and Computer Information System. I not only had rich 
+								experience in many projects using different programming languages such as Java, PHP, node.js, Ruby, and python,
+								but also different technologies such as Amazon AWS, IOS, Android, Linux System Administration. 
+							</div>
+
+							<div class="section-div-content-subdiv-bar-decorator"></div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="section-div-content-subdiv-title">
+								<p>WHAT I DO
+								</p>
+							</div>
+
+							<div class="section-div-content-subdiv-bar-decorator"></div>
+
+							<div class="section-div-content-subdiv-content">
+								I am currently working as a software developer in a digital marketing firm in Newton, MA. During the work, I am
+								heavily involved in web devlopment projects for different clients and also develop digital marketing software for the company.
+								Meanwhile, I am also involved in some research projects at BU regarding to web application development and machine learning. 
+							</div>
+							<div class="section-div-content-subdiv-bar-decorator"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="section-div">
+				<div class="section-div-title-decorator">
+					<div class="section-div-title-decorator-bar"></div>
+					<div class="section-div-title">
+						<p>PORTFOLIO</p>
+					</div>
+
+					<div class="section-div-sub-decorator-div">
+						<div class="section-div-title-decorator-subbar"></div>
+						<div class="section-div-subtitle">
+							<p><span>&#9733;</span>Websites<span>&#9733;</span></p>
+						</div>
+					</div>
+
+					<div class="section-div-content">
+						<div class="section-div-content-subdiv">
+
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							
+							<div class="website-img-div">
+								<a href="http://bookswap.space/home"><img src="images/websites/bookswap.png" /></a>
+							</div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							<div class="website-img-div">
+								<a href="http://connoisseurs.space/"><img src="images/websites/connoisseurs.png"/></a>
+							</div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							<div class="website-img-div">
+								<a href="http://www.cicdofboston.com/"><img src="images/websites/cicd.png"/></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="section-div-content">
+						<div class="section-div-content-subdiv">
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							<div class="website-img-div">
+								<a href="http://128.197.103.76/"><img src="images/websites/BU_MET_research.png" /></a>
+							</div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							<div class="website-img-div">
+								<a href="http://fu-xin-tang.com/"><img src="images/websites/fu-xin-tang.png"/></a>
+							</div>
+						</div>
+
+						<div class="section-div-content-subdiv">
+							<div class="website-img-div-overlay">
+								<p>Click Me</p>
+							</div>
+							<div class="website-img-div">
+								<a href="https://enigmatic-temple-97730.herokuapp.com"><img src="images/websites/samarth_bais_site.png"/></a>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="section-div-sub-decorator-div">
+						<div class="section-div-title-decorator-subbar"></div>
+						<div class="section-div-subtitle">
+							<p><span>&#9733;</span>Gradphics<span>&#9733;</span></p>
+						</div>
+					</div>
+
+					<div class="section-div-content">
+						<ul class="carousel">
+						  <li class="item active">
+						  	<img src="images/print_media/saint_patricks.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item active">
+						  	<img src="images/print_media/back_to_school.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/bag_sales.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/clark_collections.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/clark_holiday_shopping.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/easter_day_poster.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/FA_ny_trip.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/furniture_pickup.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/halloween.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/speaker_event.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/stock_trading.png" class="carousel-img" width="165" style="margin: 0 auto; display: block"/>
+						  </li>
+						  <li class="item">
+						  	<img src="images/print_media/summer_sales.png" class="carousel-img" width="165" style="margin: 0 auto; display: block; margin-top: 1em"/>
+						  </li>
+						</ul>
+
+						<div class="controls">
+							<a href="#" class="previous"><img src="images/left_arrow.png" alt="previous" /></a>
+							<a href="#" class="next"><img src="images/right_arrow.png" alt="next"  /></a>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="footer">
+			<div class="footer-title-decorator-bar"></div>
+				
+			<div class="footer-title">
+				<p>CONTACT</p>
+			</div>
+
+			<div class="contacts-info">
+				<p>Tel: (508)873-6360</p>
+				<p>Email: chihwu@bu.edu</p>
+
+				<p class="copyright-info">@2017 by Raymond Wu</p>
+			</div>
+
+
+		</div>
+
+		<script src="jquery-3.1.1.min.js"></script>
+		<script src="script.js"></script>
+		<script src="jquery.circular-carousel.js"></script>
+
+	</body>
+</html>
